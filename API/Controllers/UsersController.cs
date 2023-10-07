3d7d77bc7abf065bfaa08f6d50bc6dc375fba8de
aramosvizcarra <aramosvizcarra@gmail.com>
2023-10-07 12:43:17 -0400

    more progress

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+
+        return _context.Users;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+
+        return _context.Users.Find(id);
+    }
+}
