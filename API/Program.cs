40f0947b47ba3587239a9e0937c7ff9588079b3f
tito_drum <tito_drum@yahoo.com>
2023-10-21 08:34:33 -0400

    added api sample

--- API/Program.cs
+++ API/Program.cs
@@ -9,16 +9,14 @@ builder.Services.AddDbContext<DataContext>(opt=>{
 });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors();
 
 var app = builder.Build();
-
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+ app.UseCors(builder => 
+ builder.AllowAnyHeader()
+ .AllowAnyMethod()
+ .WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
